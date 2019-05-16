76d5accba569a3a235f6aa4785f28ff912daa063
Kaustubh Dixit <kdkishu335@gmail.com>
2019-05-16 15:18:44 +0530

    Removed Conflicts.

--- src/main/admin.py
+++ src/main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
+from .models import PastRecruiters, CoreTeamContacts, Volunteers, News, AlumniTestimonial, HomeImageCarousel
 from import_export.admin import ImportExportActionModelAdmin
-from .models import News, PastRecruiters
 
 
 @admin.register(News)
@@ -20,4 +20,43 @@ class PastRecruitersAdmin(admin.ModelAdmin):
 
     class Meta:
         model = PastRecruiters
+        fields = '__all__'
+
+@admin.register(HomeImageCarousel)
+class HomeImageCarouselAdmin(admin.ModelAdmin):
+    list_display = ['title', 'ordering', 'active', ]
+
+    class Meta:
+        model = HomeImageCarousel
+        fields = '__all__'
+
+@admin.register(CoreTeamContacts)
+class CoreTeamContactsAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'designation', 'active', ]
+    list_filter = ['active', 'designation', ]
+    ordering = ['order_no', ]
+
+    class Meta:
+        model = CoreTeamContacts
+        fields = '__all__'
+
+
+@admin.register(Volunteers)
+class VolunteersAdmin(admin.ModelAdmin):
+    list_display = ['name', 'year', 'active', ]
+    list_filter = ['active', ]
+
+    class Meta:
+        model = Volunteers
+        fields = '__all__'
+
+
+@admin.register(AlumniTestimonial)
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ['ranking', 'alumni_name', 'active', ]
+    list_filter = ['active', ]
+    ordering = ['ranking', ]
+
+    class Meta:
+        model = AlumniTestimonial
         fields = '__all__'
--- src/main/views.py
+++ src/main/views.py
@@ -1,16 +1,16 @@
 from django.views.generic import TemplateView
 import os
-from .models import News, PastRecruiters
+from .models import News, PastRecruiters, HomeImageCarousel
 
 
 class HomepageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(HomepageView, self).get_context_data()
-        carousel = os.listdir("staticfiles/img/homepage-carousel")
-        carousel.sort(key=lambda x: os.path.getmtime('staticfiles/img/homepage-carousel/' + x), reverse=True)
-        carousel = ['img/homepage-carousel/' + image for image in carousel]
-        context['carousel'] = carousel
+        context['carousel'] = HomeImageCarousel.objects.filter(active=True).order_by('ordering')
         context['news_list'] = News.objects.filter(active=True).order_by('order_no')
         context['companies'] = PastRecruiters.objects.filter(active=True).order_by('company_order_no')
+        context['contacts'] = CoreTeamContacts.objects.filter(active=True).order_by('order_no')
+        context['volunteers'] = Volunteers.objects.filter(active=True).order_by('order_no')
+        context['testimonial_list'] = AlumniTestimonial.objects.filter(active='True').order_by('ranking')
         return context
